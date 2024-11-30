1cbcc83d1492f2e46c9d651dd02dfb81db727aa7
gustavofbs <guguboasorte@gmail.com>
2024-11-30 17:54:13 -0300

    corrige problema da busca

--- 778. Swim in Rising Water/swimInRisingWater.py
+++ 778. Swim in Rising Water/swimInRisingWater.py
@@ -1,23 +1,18 @@
+from heapq import heappop, heappush
+
 class Solution:
     def swimInWater(self, grid):
-        from collections import deque
-
         n = len(grid)
         dirs = ((0, 1), (1, 0), (0, -1), (-1, 0))
-        seen = set()
-        queue = deque([(0, 0, grid[0][0])])
+        heap = [(grid[0][0], 0, 0)]
 
-        while queue:
-            row, col, steps = queue.popleft()
-            if (row, col) in seen:
-                continue
-            seen.add((row, col))
+        while heap:
+            steps, row, col = heappop(heap)
             if row == col == n - 1:
                 return steps
             for dx, dy in dirs:
                 x, y = row + dx, col + dy
                 if 0 <= x < n and 0 <= y < n:
-                    queue.append((x, y, max(steps, grid[x][y])))
+                    heappush(heap, (max(steps, grid[x][y]), x, y))
 
         return -1
-
