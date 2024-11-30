b61ac76f7309f3e21dcd67035ba2518429f07b7c
gustavofbs <guguboasorte@gmail.com>
2024-11-30 11:47:35 -0300

    add versao sem restricao para iniciar o desenvolvimento

---
+++ 778. Swim in Rising Water/swimInRisingWater.py
@@ -0,0 +1,14 @@
+class Solution:
+    def swimInWater(self, grid):
+        def dfs(row, col, steps):
+            n = len(grid)
+            if row == n - 1 and col == n - 1:
+                return steps
+            if row < 0 or row >= n or col < 0 or col >= n:
+                return float('inf')
+            return min(
+                dfs(row + 1, col, max(steps, grid[row][col])),
+                dfs(row, col + 1, max(steps, grid[row][col]))
+            )
+
+        return dfs(0, 0, grid[0][0])
