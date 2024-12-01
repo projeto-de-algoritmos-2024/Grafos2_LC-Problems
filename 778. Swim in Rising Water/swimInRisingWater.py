a30a936047d64c8dda6fcd25a59c572d059f9996
gustavofbs <guguboasorte@gmail.com>
2024-11-30 23:15:52 -0300

    altera a estrutura para ser aceita no lc

--- 778. Swim in Rising Water/swimInRisingWater.py
+++ 778. Swim in Rising Water/swimInRisingWater.py
@@ -1,18 +1,24 @@
-from heapq import heappop, heappush
-
 class Solution:
-    def swimInWater(self, grid):
+    def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
         dirs = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+        def isValid(row, col):
+            return 0 <= row < n and 0 <= col < n
+
         heap = [(grid[0][0], 0, 0)]
         seen = {(0, 0)}
 
         while heap:
             steps, row, col = heappop(heap)
+
             if row == col == n - 1:
                 return steps
+
             for dx, dy in dirs:
                 x, y = row + dx, col + dy
-                if 0 <= x < n and 0 <= y < n and (x, y) not in seen:
+
+                if isValid(x, y) and (x, y) not in seen:
                     seen.add((x, y))
                     heappush(heap, (max(steps, grid[x][y]), x, y))
+        
